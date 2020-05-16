720f772baee71b3190b5e5032f48d7458e49c8dc
Ammara Khan <ammarakhan63@yahoo.com>
2020-05-16 18:20:44 +0200

    Added scenarios for upcoming movies

--- features/menu.feature
+++ features/menu.feature
@@ -1,9 +1,15 @@
 Feature: As a user I want to search a movie by using different filters
 
-Background: I am on the site
-    Given that I am on the IMDB site
-    And I have pressed the menu-button to expand links of the site
+  Background: I am on the site
+    Given that I am on the IMDB website
+    And I have clicked the hamburger menu to expand links of the site
 
-Scenario: Finding top movies
+  Scenario: Find top rated movies
     When I press the "Top Rated Movies"
-    Then I should get list of the top rated movies
+    Then I should get non-empty list of the top rated movies
+
+  Scenario: Find upcoming movies
+    When I press "Coming Soon"
+    When I click "Next"
+    Then I should see movies list upcoming in next month
+  
--- step-definitions/Menu.js
+++ step-definitions/Menu.js
@@ -1,11 +1,11 @@
 let { $, sleep } = require('./funcs');
 
 module.exports = function () {
-  this.Given(/^that I am on the IMDB site$/, async function () {
+  this.Given(/^that I am on the IMDB website$/, async function () {
     await helpers.loadPage('https://www.imdb.com');
   });
 
-  this.Given(/^I have pressed the menu-button to expand links of the site$/, async function () {
+  this.Given(/^I have clicked the hamburger menu to expand links of the site$/, async function () {
     let menuButton = await $('#imdbHeader-navDrawerOpen--desktop');
     await menuButton.click();
   });
@@ -14,5 +14,28 @@ module.exports = function () {
     let topRatedLink = await driver.wait(until.elementLocated(By.linkText(link)));
     await topRatedLink.click();
     expect(topRatedLink, 'Unable to find Top Rated Movies link in menu items');
+  });
+
+  this.Then(/^I should get non-empty list of the top rated movies$/, async function () {
+    let list = await $('.posterColumn');
+    let listLength = [...list].length;
+    expect(listLength).to.not.equal(0, 'Top Rated movies list cannot be empty');
+  });
+
+  this.When(/^I press "([^"]*)"$/, async function (link) {
+    let comingSoonLink = await driver.wait(until.elementLocated(By.linkText(link)));
+    await comingSoonLink.click();
+    expect(comingSoonLink, 'Unable to find Coming Soon link in menu items');
+  });
+
+  this.When(/^I click "([^"]*)"$/, async function (next) {
+    driver.findElement(By.linkText(next)).click();
+    await sleep(5000);
+  });
+
+  this.Then(/^I should see movies list upcoming in next month$/, async function () {
+    let item = await driver.findElement(By.id('img_primary'));
+    console.log(item);
+    assert(item !== null, 'Upcoming movies list can not be empty for June')
   });
 }
